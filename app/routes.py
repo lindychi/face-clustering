a5cbb74a3da34ff82025b9a581b24e4e86320598
hanchi <hanchi@tosky.co.kr>
2023-10-16 23:53:46 +0900

    feat: 로그인 페이지 추가

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,17 @@
 from app import app, db
 from app.models import User
 import face_recognition
-from flask import request, jsonify
+from flask import request, render_template
 
-# 라우트 및 뷰 함수
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.check_password(password):
+            return f"Logged in as {user.username}"
+        else:
+            return "Invalid username or password"
+    return render_template('login.html')
